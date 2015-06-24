433580f18ac97ea6076184c43dcac233f89d189e
Oleksiy Tkachenko <olexiy.tkachenko@gmail.com>
2015-06-24 14:41:06 +0300

    fixes

--- scripts/serialCtrl.js
+++ scripts/serialCtrl.js
@@ -6,31 +6,33 @@ linguahack.factory('fsParse', ['$http', fsParse]);
 
 function fsParse($http) {
   return function(serial, file, episodeNumber) {
-    return $http.get(
-      'http://fs.to/video/serials/view_iframe/' + serial.fsto.id,
-      {
+    return $http({
+      method: "GET",
+      url: 'http://fs.to/video/serials/view_iframe/' + serial.fsto.id,
+      params: {
         play: 1,
         file: file.file_id
       }
-    )
+    })
     .then(function(body) {
       var elem = $(body.data);
 
-      var files = [];
+      var file;
       
       $('.b-aplayer__popup-series-episodes > ul > li > a', elem).each(function(i, elem) {
+        if (file) return;
         var episode = JSON.parse($(elem).attr('data-file'));
-        if (+episode.fsData.file_series === episodeNumber + 1) {
-          files.push({
+        if (+episode.fsData.file_series === +episodeNumber) {
+          file = {
             number: +episode.fsData.file_series,
             quality: episode.fsData.file_quality,
             file_id: +episode.fsData.file_id,
             link: episode.url
-          });
+          };
         }
       });
 
-      return files[0];
+      return file;
     });
   };
 }
@@ -51,11 +53,14 @@ linguahack.controller('SerialCtrl', function($scope, $stateParams, Api, getImage
 
 
   $scope.select_season = function(number) {
+    $scope.serial.seasons[number].episodes.sort(function(first, second) {
+      return first.number - second.number;
+    });
     $scope.season = $scope.serial.seasons[number];
     $scope.select_episode(0);
   };
   $scope.select_episode = function(number) {
-    return fsParse($scope.serial, $scope.season.episodes[number].fsto.files[0], number)
+    return fsParse($scope.serial, $scope.season.episodes[number].fsto.files[0], $scope.season.episodes[number].number)
     .then(function(result) {
       console.log(result);
       $scope.season.episodes[number].link = 'http://fs.to' + result.link;
